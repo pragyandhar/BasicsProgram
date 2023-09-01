ba4e0673cdc2065e92192f84b2629ba496b1f781
Pragyan <142593392+pragyandhar@users.noreply.github.com>
2023-08-31 23:17:18 -0700

    Delete Class Assignment-1(1st Sept 23) directory

--- Class Assignment-1(1st Sept 23)/timeconverter.py
+++
@@ -1,9 +0,0 @@
-# Question-6: WAP that converts the given number of seconds into hours, minutes and seconds
-
-# INPUT SECTION
-a = int(input("Enter time in seconds: "))
-
-# LOGIC SECTION
-
-
-# DISPLAY SECTION
