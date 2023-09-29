db08479b403a21ecbb3aeb922c18920235280a59
Pragyan <142593392+pragyandhar@users.noreply.github.com>
2023-09-28 22:52:32 -0700

    Delete anagram_detection.py

--- anagram_detection.py
+++
@@ -1,9 +0,0 @@
-def anagrams():
-    if sorted(str1)==sorted(str2):
-        print("Anagram")
-    else:
-        print("Not Anagram")
-
-str1 = input("::::(wite in small letters only)::::")
-str2 = input("::::(wite in small letters only)::::")
-call = anagrams()
