6e727c111b24803c36dee38967af9e8fcba68079
Pragyan <142593392+pragyandhar@users.noreply.github.com>
2023-10-26 23:03:06 -0700

    Delete find the repeated items of a tuple.py

--- find the repeated items of a tuple.py
+++
@@ -1,8 +0,0 @@
-#WAP to find the repeated items of a tuple
-a = (1,1,2,3,2,3)
-reg = []
-for i in a:
-    if a.count(i)>1 and i not in reg:
-        print("The element which is repeated is: ", i)
-        reg.append(i)
-
